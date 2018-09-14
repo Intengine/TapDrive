e0e85f9fe90c62eec8545f6922c982e57c03d15a
Intengine <studio@intengine.pl>
2018-09-14 11:14:37 +0200

    Initialized createRoadStrip() func
    Created leftRoadStrip and rightRoadStrip

--- TapDrive/TapDrive/GameScene.swift
+++ TapDrive/TapDrive/GameScene.swift
@@ -10,7 +10,40 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var leftCar = SKSpriteNode()
+    var rightCar = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setUp()
+        createRoadStrip()
+    }
+    
+    func setUp() {
+        leftCar = self.childNode(withName: "leftCar") as! SKSpriteNode
+        rightCar = self.childNode(withName: "rightCar") as! SKSpriteNode
+    }
+    
+    func createRoadStrip() {
+        let leftRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        leftRoadStrip.strokeColor = SKColor.white
+        leftRoadStrip.fillColor = SKColor.white
+        leftRoadStrip.alpha = 0.4
+        leftRoadStrip.name = "leftRoadStrip"
+        leftRoadStrip.zPosition = 10
+        leftRoadStrip.position.x = -187.5
+        leftRoadStrip.position.y = 700
+        addChild(leftRoadStrip)
         
+        let rightRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        rightRoadStrip.strokeColor = SKColor.white
+        rightRoadStrip.fillColor = SKColor.white
+        rightRoadStrip.alpha = 0.4
+        rightRoadStrip.name = "rightRoadStrip"
+        rightRoadStrip.zPosition = 10
+        rightRoadStrip.position.x = 187.5
+        rightRoadStrip.position.y = 700
+        addChild(rightRoadStrip)
     }
 }
