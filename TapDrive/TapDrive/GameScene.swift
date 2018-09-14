beb6f2818776f4597701a935f423d1e25a293a7e
Intengine <studio@intengine.pl>
2018-09-14 12:02:20 +0200

    Created road strips
    Created both left and right road strips. Made them move in loop.

--- TapDrive/TapDrive/GameScene.swift
+++ TapDrive/TapDrive/GameScene.swift
@@ -17,7 +17,11 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         setUp()
-        createRoadStrip()
+        Timer.scheduledTimer(timeInterval: TimeInterval(0.1), target: self, selector: #selector(GameScene.createRoadStrip), userInfo: nil, repeats: true)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        showRoadStrip()
     }
     
     func setUp() {
@@ -25,7 +29,7 @@ class GameScene: SKScene {
         rightCar = self.childNode(withName: "rightCar") as! SKSpriteNode
     }
     
-    func createRoadStrip() {
+    @objc func createRoadStrip() {
         let leftRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
         leftRoadStrip.strokeColor = SKColor.white
         leftRoadStrip.fillColor = SKColor.white
@@ -45,5 +49,17 @@ class GameScene: SKScene {
         rightRoadStrip.position.x = 187.5
         rightRoadStrip.position.y = 700
         addChild(rightRoadStrip)
+    }
+    
+    func showRoadStrip() {
+        enumerateChildNodes(withName: "leftRoadStrip", using: {(roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        })
+        
+        enumerateChildNodes(withName: "rightRoadStrip", using: {(roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        })
     }
 }
